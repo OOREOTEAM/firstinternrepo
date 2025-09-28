620ec7a3cf9dcbe78b423c1a04c9d8f9a28b24a5
larysalozynska <llozy@softserveinc.com>
2025-09-28 15:03:20 +0000

    Fixed issues with GitHub Actions

--- week1/rhnatyshyn/flask/birdproject/auth.py
+++ week1/rhnatyshyn/flask/birdproject/auth.py
@@ -1,11 +1,11 @@
+import os
 from flask import Blueprint, render_template, redirect, url_for, request, flash, current_app,abort
 from werkzeug.security import generate_password_hash, check_password_hash
 from werkzeug.utils import secure_filename
 from flask_login import login_user, logout_user, login_required, current_user
-from .models import User, Photo, Hunter, Role
-from . import db
 from passlib.hash import sha256_crypt
-import os
+from .models import User, Photo, Hunter
+from . import db
 
 auth = Blueprint('auth', __name__)
 
