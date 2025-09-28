9dc53f71151b1ef1b366584e05ba363a919d744a
oalexyeyev <alexyeyev@gmail.com>
2025-09-28 23:12:15 +0300

    import request

--- week1/rhnatyshyn/flask/birdproject/main.py
+++ week1/rhnatyshyn/flask/birdproject/main.py
@@ -1,4 +1,4 @@
-from flask import Flask, Blueprint , render_template
+from flask import Flask, Blueprint , render_template, request
 from flask_login import login_required, current_user
 from .models import Photo, Hunter
 import hashlib
