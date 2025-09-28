340b5aae511b62f7fd02843ba7a449c267cc2365
oalexyeyev <alexyeyev@gmail.com>
2025-09-28 23:22:16 +0300

    import redirect

--- week1/rhnatyshyn/flask/birdproject/main.py
+++ week1/rhnatyshyn/flask/birdproject/main.py
@@ -1,4 +1,4 @@
-from flask import Flask, Blueprint , render_template, request
+from flask import Flask, Blueprint , render_template, request, redirect
 from flask_login import login_required, current_user
 from .models import Photo, Hunter
 import hashlib
